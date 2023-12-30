fc5fcbc82be86fa3bca6399efc0e601d386853b3
Vineet Jadhav <vineet290403@gmail.com>
2023-12-30 20:58:02 +0530

    React states and components

---
+++ Week_4/counter/src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "Go Home",
+      description: "abc",
+      completed: "false",
+    },
+    {
+      title: "Go Gym",
+      description: "abc",
+      completed: "false",
+    },
+  ]);
+
+  return (
+    <div>
+      {todos.map((todo) => {
+        return (
+          <Todo
+            title={todo.title}
+            description={todo.description}
+            completed={todo.completed}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+//*Componenet
+function Todo(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h1>{props.description}</h1>
+      <h1>{props.completed}</h1>
+    </div>
+  );
+}
+export default App;
