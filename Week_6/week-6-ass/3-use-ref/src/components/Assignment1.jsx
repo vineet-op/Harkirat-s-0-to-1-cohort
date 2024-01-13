2b467019f9c48491d417435f44a389ae80de02d3
Vineet Jadhav <vineet290403@gmail.com>
2024-01-13 15:58:15 +0530

    Added UseRef Assignments

--- Week_6/week-6-ass/3-use-ref/src/components/Assignment1.jsx
+++ Week_6/week-6-ass/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
+  const refElement = useRef();
 
-    useEffect(() => {
+  useEffect(() => {
+    refElement.current.focus();
+    console.log("Focus on UseEffect");
+  }, []);
 
-    }, []);
+  const handleButtonClick = () => {
+    refElement.current.focus();
+    console.log("Focus on Button Click");
+  };
 
-    const handleButtonClick = () => {
-
-    };
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter text here" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <input ref={refElement} type="text" placeholder="Enter text here" />
+      <button onClick={handleButtonClick}>Focus Input</button>
+    </div>
+  );
+}
--- Week_6/week-6-ass/3-use-ref/src/components/Assignment2.jsx
+++ Week_6/week-6-ass/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useCallback } from 'react';
-
-// Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
+import React, { useState, useRef } from "react";
 
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+  const [Count, setCount] = useState(0);
 
-    const handleReRender = () => {
-        // Update state to force re-render
-        forceRender(Math.random());
-    };
+  const renderCount = useRef(0);
 
-    return (
-        <div>
-            <p>This component has rendered {0} times.</p>
-            <button onClick={handleReRender}>Force Re-render</button>
-        </div>
-    );
-};
+  const handleReRender = () => {
+    renderCount.current = renderCount.current + 1;
+    setCount((prev) => prev + 1);
+    console.log("This component has rendered", renderCount.current, "times.");
+  };
+
+  return (
+    <div>
+      <p>This component has rendered {Count} times.</p>
+      <button onClick={handleReRender}>Force Re-render</button>
+    </div>
+  );
+}
