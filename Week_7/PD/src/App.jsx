ebfd4edb24bb9453044e4faf5962a725b4661c96
Vineet Jadhav <vineet290403@gmail.com>
2024-01-30 18:44:19 +0530

    Context API Done

---
+++ Week_7/PD/src/App.jsx
@@ -0,0 +1,54 @@
+import { useContext, useState } from "react";
+import { CountContext } from "./Context";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    //Wrap anyone who wants to use the teleported value in Provider
+    <div>
+      <CountContext.Provider value={{ count, setCount }}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  );
+}
+
+function Count({ setCount }) {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons setCount={setCount} />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return <div>{count}</div>;
+}
+
+function Buttons() {
+  const { count, setCount } = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
