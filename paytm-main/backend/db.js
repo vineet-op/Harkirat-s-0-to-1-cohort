27204ad0b60efd36cc20e060d45215be535296cb
Vineet Jadhav <vineet290403@gmail.com>
2024-02-04 16:14:13 +0530

    Added Stage 5 PayTm

---
+++ paytm-main/backend/db.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://massleo100:PayTmClone@paytm.uqngsyj.mongodb.net/"
+);
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {
+  User,
+};
