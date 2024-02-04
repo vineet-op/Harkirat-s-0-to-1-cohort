41e4db531abc02abc94bcdb968f00826b2ad33cb
Vineet Jadhav <vineet290403@gmail.com>
2024-02-04 16:59:08 +0530

    created signup and signin routes

--- paytm-main/backend/index.js
+++ paytm-main/backend/index.js
@@ -8,3 +8,5 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/vi", rootRouter);
+
+app.listen(3000);
--- paytm-main/backend/routes/user.js
+++ paytm-main/backend/routes/user.js
@@ -1,5 +1,82 @@
 const express = require("express");
-
+const zod = require("zod");
+const { User } = require("../db");
 const router = express.Router();
+const { JWT_SECRET } = require("../config");
+
+const SignupSchema = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+});
+
+router.post("/signup", async function (req, res) {
+  const { success } = SignupSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect Input",
+    });
+  }
+
+  const existingUser = User.findOne({
+    username: req.body.username,
+  });
+
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email Already taken",
+    });
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  });
+
+  const UserId = user._id;
+
+  const token = jwt.sign({ UserId }, JWT_SECRET);
+
+  res.json({
+    message: "User created succesfully",
+    token: token,
+  });
+});
+
+const SigninSchema = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+
+router.post("/signin", async function (req, res) {
+  const { success } = SigninSchema.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect username or password",
+    });
+  }
+
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const token = jwt.sign({ UserId: user._id }, JWT_SECRET);
+
+    res.json({
+      token: token,
+    });
+    return;
+  }
+
+  res.status(411).json({
+    message: "Error While logging in",
+  });
+});
 
 module.exports = router;
